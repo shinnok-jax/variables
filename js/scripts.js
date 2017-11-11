f17e3b7cc2ec1d3210ec45bdb7cc07e74617c8c7
Marcin Leissa <jax.shinnok@gmail.com>
2017-11-11 12:33:34 +0100

    poprawka

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var a = 5;
 var h = 2;
-var triangleArea = [a*h] / 2;
+var triangleArea = (a*h) / 2;
 alert('Pole trójkąta z podstawą a: ' + a + ' oraz wyskością h: ' + h + ' jest równe: ' + triangleArea);
 
